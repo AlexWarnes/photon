0121cfaf5a184875ba1c84580f37b990fa7757ff
Alex Warnes <alex.s.warnes@gmail.com>
2022-06-09 22:51:18 -0500

    just disable ssr for now

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,15 +1,8 @@
 import type { Handle } from '@sveltejs/kit';
 
 // Until this issue gets resolved, must disable SSR for lamina scenes
-const nonSSRPaths: string[] = [
-  "the-expanse",
-  "lamina-noisy-spheres"
-]
+// const nonSSRPaths: string[] = ['the-expanse', 'lamina-noisy-spheres'];
 
 export const handle: Handle = async ({ event, resolve }) => {
-  if (nonSSRPaths.some(path => event.url.pathname.includes(path))) {
-    return await resolve(event, { ssr: false });
-  }
-
-  return await resolve(event);
+	return await resolve(event, { ssr: false });
 };
