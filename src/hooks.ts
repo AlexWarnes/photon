274032ab1a5b0dd93ac01a72e4de65334a27f1b7
Alex Warnes <alex.s.warnes@gmail.com>
2022-06-09 22:48:41 -0500

    disable ssr for lamina routes

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,7 +2,8 @@ import type { Handle } from '@sveltejs/kit';
 
 // Until this issue gets resolved, must disable SSR for lamina scenes
 const nonSSRPaths: string[] = [
-  "the-expanse"
+  "the-expanse",
+  "lamina-noisy-spheres"
 ]
 
 export const handle: Handle = async ({ event, resolve }) => {
