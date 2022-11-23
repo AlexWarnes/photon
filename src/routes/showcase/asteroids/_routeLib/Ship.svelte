7701b25f6cd2046560fb285e4c0769a5d3e97ce2
Alex Warnes <alex.s.warnes@gmail.com>
2022-11-23 14:25:56 -0600

    add ship citation

--- src/routes/showcase/asteroids/_routeLib/Ship.svelte
+++ src/routes/showcase/asteroids/_routeLib/Ship.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	/**
+	 * Model Information:
+		* title:	low poly space ship
+		* source:	https://sketchfab.com/3d-models/low-poly-space-ship-587941c9c11742c6b82dfb99e7b210b9
+		* author:	chrisonciuconcepts (https://sketchfab.com/chrisonciuconcepts)
+
+		Model License:
+		* license type:	CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+		* requirements:	Author must be credited. Commercial use is allowed.
+
+		If you use this 3D model in your project be sure to copy paste this credit wherever you share it:
+		This work is based on "low poly space ship" (https://sketchfab.com/3d-models/low-poly-space-ship-587941c9c11742c6b82dfb99e7b210b9) by chrisonciuconcepts (https://sketchfab.com/chrisonciuconcepts) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+	*/
+
 	import { GLTF } from '@threlte/extras';
 
 	// import Reticule from './Reticule.svelte';
